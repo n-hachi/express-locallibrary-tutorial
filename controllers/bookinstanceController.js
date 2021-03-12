f3bf12d77a91ce4893d0e50b3df4f8ea9538b105
Nozomu Hatta <nozomuhatta@gmail.com>
2021-03-13 08:03:06 +0900

    Add /catalog/bookinstance/:id page

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -12,8 +12,20 @@ exports.bookinstance_list = function(req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+    var id = req.params.id;
+    BookInstance.findById(id)
+    .populate('book')
+    .exec(function(err, bookinstance){
+        if(err){return next(err);}
+        if(bookinstance == null){
+            var err = new Error('Book copy not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('bookinstance_detail', {title: 'Copy', bookinstance: bookinstance});
+    });
 };
 
 // Display BookInstance create form on GET.
